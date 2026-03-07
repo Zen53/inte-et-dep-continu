550becb39fc659c0cd3acf947fe31e05df852266
Zen53 <elias93.benrejem@gmail.com>
2026-03-07 05:40:48 +0100

    feat: (minor 1.1.0)

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,7 @@
 import App from './App.jsx';
 import PersonForm from './components/PersonForm.jsx';
+import { validatePerson, validateAge, validateZipCode, validateCity, validateName, validateEmail } from './domain/validator.js';
+import { errorMessages, getErrorMessage } from './utils/errorMessages.js';
 
 export default App;
-export { PersonForm };
+export { PersonForm, validatePerson, validateAge, validateZipCode, validateCity, validateName, validateEmail, errorMessages, getErrorMessage };
